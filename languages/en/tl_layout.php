2d6baadc5a062156a154ee45f66b9413cc636445
fatcrobat <Rico.kaltofen@gmail.com>
2012-01-31 18:35:27 +0100

    added english language file

---
+++ languages/en/tl_layout.php
@@ -0,0 +1,53 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+/**
+ * Fields
+ */
+
+$GLOBALS['TL_LANG']['tl_layout']['addjQuery'] = array('Enable jQuery', 'Enable jQuery as an as an additional Javascript framework');
+$GLOBALS['TL_LANG']['tl_layout']['jQueryVersion'] = array('jQuery version', 'Determine which version of jQuery to use.');
+$GLOBALS['TL_LANG']['tl_layout']['jQuerySource'] = array('jQuery source', 'JQuery is to be loaded locally or via a CDN.');
+$GLOBALS['TL_LANG']['tl_layout']['jQuerySourceOptions']['local'] = 'Local';
+$GLOBALS['TL_LANG']['tl_layout']['jQuerySourceOptions']['google'] = 'Google Ajax API CDN';
+$GLOBALS['TL_LANG']['tl_layout']['jQuerySourceOptions']['microsoft'] = 'Microsoft CDN';
+$GLOBALS['TL_LANG']['tl_layout']['jQuerySourceOptions']['jquery'] = 'jQuery CDN'; 
+$GLOBALS['TL_LANG']['tl_layout']['jQueryScripts'] = array('jQuery scripts', 'Combined additional jQuery scripts integrated inside the template directory.');
+$GLOBALS['TL_LANG']['tl_layout']['jQuerycombineScripts'] = array('Combine jQuery scripts', 'To speed up the loading time of your website, in live mode it is recommended to hold all jQuery scripts together into a single file.');
+$GLOBALS['TL_LANG']['tl_layout']['addjQueryUI'] = array('Enable jQuery UI', 'Enable jQuery user interface library.');
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIVersion'] = array('jQuery UI version', 'Determine which version of jQuery UI to use.');
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUISource'] = array('jQuery UI source', 'JQuery UI is to be loaded locally or via a CDN.');
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUISourceOptions']['local'] = 'Local';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUISourceOptions']['google'] = 'Google Ajax API CDN';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUISourceOptions']['microsoft'] = 'Microsoft CDN';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUITheme'] = array('jQuery UI Theme', 'Determine which jQuery UI theme to use for. If you selected as "jQuery UI Source" the option "local" you can download the themes manually from http://code.google.com/p/jquery-ui/downloads/list and store them inside the current template folder, or create your very own jQuery UI theme.');
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['base'] = 'Base';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['black-tie'] = 'Black Tie';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['blitzer'] = 'Blitzer';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['cupertino'] = 'Cupertino';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['dark-hive'] = 'Dark Hive';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['dot-luv'] = 'Dot Luv';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['eggplant'] = 'Eggplant';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['excite-bike'] = 'Excite Bike';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['flick'] = 'Flick';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['hot-sneaks'] = 'Hot Sneaks';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['humanity'] = 'Humanity';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['le-frog'] = 'Le Frog';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['mint-choc'] = 'Mint Chocolate';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['overcast'] = 'Overcast';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['pepper-grinder'] = 'Peper Grinder';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['redmond'] = 'Redmond';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['smoothness'] = 'Smoothness';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['south-street'] = 'South Street';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['start'] = 'Start';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['sunny'] = 'Sunny';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['swanky-purse'] = 'Swanky Purse';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['trontastic'] = 'Trontastic';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['ui-darkness'] = 'UI Darkness';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['ui-lightness'] = 'UI Lightness';
+$GLOBALS['TL_LANG']['tl_layout']['jQueryUIThemeOptions']['vader'] = 'Vader';
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_layout']['jQuery_legend'] = 'jQuery Support';
+
